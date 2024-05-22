0ed54a0695d3b7fe2221dab3965ae11b12e014ca
KelaxaVav <kelaxa.vav@gmail.com>
2024-05-22 15:51:15 +0530

    vehicla class edit

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -53,6 +53,28 @@ function Dashboard(props) {
                             <div className="card-body">
                                 <div className="row align-items-center">
                                     <div className="col-6">
+                                        <span className="text-muted mb-3 lh-1 d-block text-truncate">register Payment</span>
+                                        <h4 className="mb-3">
+                                            <span className="counter-value">11780.00</span>
+                                        </h4>
+                                    </div>
+                                    <div className="col-6">
+                                        <div id="mini-chart2" data-colors="[&quot;#5156be&quot;]" className="apex-charts mb-2" />
+                                    </div>
+                                </div>
+                                <div className="text-nowrap">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-md-6">
+                        {/* card */}
+                        <div className="card card-h-100">
+                            {/* card body */}
+                            <div className="card-body">
+                                <div className="row align-items-center">
+                                    <div className="col-6">
                                         <span className="text-muted mb-3 lh-1 d-block text-truncate">Re Exam Payment</span>
                                         <h4 className="mb-3">
                                             <span className="counter-value">12000.00</span>
@@ -92,29 +114,7 @@ function Dashboard(props) {
                             </div>{/* end card body */}
                         </div>{/* end card */}
                     </div>{/* end col */}
-                    <div className="col-xl-3 col-md-6">
-                        {/* card */}
-                        <div className="card card-h-100">
-                            {/* card body */}
-                            <div className="card-body">
-                                <div className="row align-items-center">
-                                    <div className="col-6">
-                                        <span className="text-muted mb-3 lh-1 d-block text-truncate">Profit Ration</span>
-                                        <h4 className="mb-3">
-                                            <span className="counter-value" data-target="12.57">0</span>%
-                                        </h4>
-                                    </div>
-                                    <div className="col-6">
-                                        <div id="mini-chart4" data-colors="[&quot;#5156be&quot;]" className="apex-charts mb-2" />
-                                    </div>
-                                </div>
-                                <div className="text-nowrap">
-                                    {/* <span className="badge badge-soft-success text-success">+2.95%</span>
-                            <span className="ms-1 text-muted font-size-13">Since last week</span> */}
-                                </div>
-                            </div>{/* end card body */}
-                        </div>{/* end card */}
-                    </div>{/* end col */}
+                   
                 </div>{/* end row*/}
 
 
