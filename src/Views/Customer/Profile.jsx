1db012767de793df41092e89a55a571d933e23ce
KelaxaVav <kelaxa.vav@gmail.com>
2024-05-27 17:17:20 +0530

    Customer profile

--- src/Views/Customer/Profile.jsx
+++ src/Views/Customer/Profile.jsx
@@ -16,23 +16,21 @@ const Profile = () => {
               </div>
               <div className="flex-grow-1">
                 <div>
-                  <h5 className="font-size-16 mb-1">Phyllis Gatlin</h5>
-                  <p className="text-muted font-size-13">Full Stack Developer</p>
+                  <h5 className="font-size-16 mb-1">Saravanan Meenatchi</h5>
+                  <p className="text-muted font-size-13">Vehicle Class (A1,B,B1)</p>
                   <div className="d-flex flex-wrap align-items-start gap-2 gap-lg-3 text-muted font-size-13">
-                    <div><i className="mdi mdi-circle-medium me-1 text-success align-middle" />Development</div>
-                    <div><i className="mdi mdi-circle-medium me-1 text-success align-middle" />phyllisgatlin@minia.com</div>
+                    <div><i className="mdi mdi-circle-medium me-1 text-success align-middle" />077 123 4567</div>
+                    <div><i className="mdi mdi-circle-medium me-1 text-success align-middle" />072 456 1258</div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-sm-auto order-1 order-sm-2">
-            <div className="d-flex align-items-start justify-content-end gap-2">
+            <div className="d-flex align-items-center justify-content-end gap-2">
+                <label htmlFor="" className='form-label '>Reg.No :- 1900</label>
               <div>
-                <button type="button" className="btn btn-soft-light"><i className="me-1" /> Message</button>
-              </div>
-              <div>
-                <div className="dropdown">
+                {/* <div className="dropdown">
                   <button className="btn btn-link font-size-16 shadow-none text-muted dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <i className="bx bx-dots-horizontal-rounded" />
                   </button>
@@ -41,20 +39,23 @@ const Profile = () => {
                     <li><a className="dropdown-item" href="#">Another action</a></li>
                     <li><a className="dropdown-item" href="#">Something else here</a></li>
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
         </div>
         <ul className="nav nav-tabs-custom card-header-tabs border-top mt-4" id="pills-tab" role="tablist">
           <li className="nav-item">
-            <a className="nav-link px-3 active" data-bs-toggle="tab" href="#overview" role="tab">Overview</a>
+            <a className="nav-link px-3 active" data-bs-toggle="tab" href="#overview" role="tab">Personal Detail</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link px-3" data-bs-toggle="tab" href="#about" role="tab">About</a>
+            <a className="nav-link px-3" data-bs-toggle="tab" href="#about" role="tab">Exam</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link px-3" data-bs-toggle="tab" href="#post" role="tab">Post</a>
+            <a className="nav-link px-3" data-bs-toggle="tab" href="#post" role="tab">Trial</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link px-3" data-bs-toggle="tab" href="#payment" role="tab">Payment</a>
           </li>
         </ul>
       </div>
@@ -65,9 +66,35 @@ const Profile = () => {
       <div className="tab-pane active" id="overview" role="tabpanel">
         <div className="card">
           <div className="card-header">
-            <h5 className="card-title mb-0">About</h5>
+            <h5 className="card-title mb-0">Customer Personal Details</h5>
           </div>
           <div className="card-body">
+            <dl class="row mb-0">
+              <dt class="col-sm-2">Full Name :-</dt>
+              <dd class="col-sm-4">Saravanan Meenatchi</dd>
+
+              <dt class="col-sm-2">NIC No :-</dt>
+              <dd class="col-sm-4">9989653214V</dd>
+
+              <dt class="col-sm-2">Address :-</dt>
+              <dd class="col-sm-4">Kovil Road kurmankadu vavuniya</dd>
+
+              <dt class="col-sm-2">Date of Birth :-</dt>
+              <dd class="col-sm-4">1999-01-01</dd>
+
+              <dt class="col-sm-2">Age :-</dt>
+              <dd class="col-sm-4">26</dd>
+
+              <dt class="col-sm-2">Phone No :-</dt>
+              <dd class="col-sm-4"> 
+                <dl class="row mb-0">
+                  <dd class="col-sm-12">077 125 4895 </dd>
+                  <dd class="col-sm-12">077 125 4895 </dd>
+
+                </dl>
+              </dd>
+
+            </dl>
           </div>
           {/* end card body */}
         </div>
@@ -76,9 +103,30 @@ const Profile = () => {
       <div className="tab-pane" id="about" role="tabpanel">
         <div className="card">
           <div className="card-header">
-            <h5 className="card-title mb-0">About</h5>
+            <h5 className="card-title mb-0">Exam</h5>
           </div>
-          <div className="card-body">
+          <div className="card-body table-responsive ">
+          <table id="dataTable" className="table table-bordered table-responsive nowrap w-100 ">
+            <thead>
+              <tr>
+                <th style={{minWidth: "200px", maxWidth:"50px",alignContent:'stretch'}}>Exam Date</th>
+                <th style={{minWidth: "200px", maxWidth:"50px",alignContent:'stretch'}}>Exam Type</th>
+                <th style={{alignContent:'stretch'}}>Status</th>
+                <th style={{alignContent:'stretch'}}>Amount</th>
+                <th style={{alignContent:'stretch'}}>Paid Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2024-01-01</td>
+                <td>Direct</td>
+                <td>Pass</td> 
+                <td>32000</td> 
+                <td>32000</td> 
+               
+              </tr>
+            </tbody>
+          </table>
           </div>
           {/* end card body */}
         </div>
@@ -88,15 +136,103 @@ const Profile = () => {
       <div className="tab-pane" id="post" role="tabpanel">
         <div className="card">
           <div className="card-header">
-            <h5 className="card-title mb-0">Post</h5>
+            <h5 className="card-title mb-0">Trial</h5>
           </div>
-          <div className="card-body">
+          <div className="card-body table-responsive ">
+          <table id="dataTable" className="table table-bordered table-responsive nowrap w-100 ">
+            <thead>
+              <tr>
+                <th style={{minWidth: "200px", maxWidth:"50px",alignContent:'stretch'}}>Exam Date</th>
+                <th style={{minWidth: "200px", maxWidth:"50px",alignContent:'stretch'}}>Exam Type</th>
+                <th style={{alignContent:'stretch'}}>Trial Date</th>
+                <th style={{alignContent:'stretch'}}>status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2024-01-01</td>
+                <td>Normal</td>
+                <td>2024-02-01</td> 
+                <td>pass</td> 
+               
+              </tr>
+            </tbody>
+          </table>
           </div>
           {/* end card body */}
         </div>
         {/* end card */}
       </div>
       {/* end tab pane */}
+      <div className="tab-pane" id="payment" role="tabpanel">
+        <div className="card">
+          <div className="card-header">
+            <h5 className="card-title mb-0">Payment</h5>
+          </div>
+          <div className="card-body table-responsive ">
+         <table className="table table-nowrap align-middle mb-0">
+  <thead>
+    <tr>
+      <th style={{width: 70}}>No.</th>
+      <th>Date</th>
+      <th>Item</th>
+      <th className="text-end" style={{width: 120}}>Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row" className="text-center" colSpan={4}>Charges </th>
+    </tr>
+    <tr>
+      <th scope="row">01</th>
+      <td><h5 className="font-size-15 mb-1">2024-01-01</h5></td>
+      <td >Application Payment</td>
+      <td className="text-end">32000.00</td>
+    </tr>
+
+    <tr>
+      <th scope="row">01</th>
+      <td><h5 className="font-size-15 mb-1">2024-01-01</h5></td>
+      <td >Re Exam (2024-01-05)</td>
+      <td className="text-end">550.00</td>
+    </tr>
+    <tr>
+      <th scope="row" className="text-center" colSpan={4}>Payment</th>
+    </tr>
+    <tr>
+      <th scope="row">01</th>
+      <td><h5 className="font-size-15 mb-1">2024-01-01</h5></td>
+      <td >Application Payment</td>
+      <td className="text-end">3000.00</td>
+    </tr>
+    <tr>
+      <th scope="row">01</th>
+      <td><h5 className="font-size-15 mb-1">2024-01-01</h5></td>
+      <td >Application Payment</td>
+      <td className="text-end">5000.00</td>
+    </tr>
+    <tr>
+      <th scope="row" colSpan={3} className=" text-end">
+        Total Payments</th>
+      <td className=" text-end">8000.00</td>
+    </tr>
+    <tr>
+      <th scope="row" colSpan={3} className="border-0 text-end">Total charges</th>
+      <td className="border-0 text-end">32550.00</td>
+    </tr>
+   
+    <tr>
+      <th scope="row" colSpan={3} className="border-0 text-end">Amount Due</th>
+      <td className="border-0 text-end"><h4 className="m-0">24550.00</h4></td>
+    </tr>
+  </tbody>
+</table>
+
+          </div>
+          {/* end card body */}
+        </div>
+        {/* end card */}
+      </div>
     </div>
     {/* end tab content */}
   </div>
@@ -104,16 +240,25 @@ const Profile = () => {
   <div className="col-xl-3 col-lg-4">
     {/* end card */}
     <div className="card">
+      <div className="card-header">
+        <h5 className="card-title ">Medical Detail</h5>
+      </div>
       <div className="card-body">
-        <h5 className="card-title mb-3">Portfolio</h5>
         <div>
           <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#" className="py-2 d-block text-muted"><i className="mdi mdi-web text-primary me-1" /> Website</a>
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">Certificate No</h5>
+                  <p className="font-size-13 text-muted mb-0">T024223</p>
             </li>
-            <li>
-              <a href="#" className="py-2 d-block text-muted"><i className="mdi mdi-note-text-outline text-primary me-1" /> Blog</a>
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">Date</h5>
+                  <p className="font-size-13 text-muted mb-0">2024-02-25</p>
             </li>
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">Note</h5>
+                  <p className="font-size-13 text-muted mb-0">O+, 56kg</p>
+            </li>
+           
           </ul>
         </div>
       </div>
@@ -121,50 +266,26 @@ const Profile = () => {
     </div>
     {/* end card */}
     <div className="card">
+      <div className="card-header">
+        <h5 className="card-title ">License Detail</h5>
+      </div>
       <div className="card-body">
-        <h5 className="card-title mb-3">Similar Profiles</h5>
-        <div className="list-group list-group-flush">
-          <a href="#" className="list-group-item list-group-item-action">
-            <div className="d-flex align-items-center">
-              <div className="avatar-sm flex-shrink-0 me-3">
-                <img src="assets/images/users/avatar-1.jpg" alt className="img-thumbnail rounded-circle" />
-              </div>
-              <div className="flex-grow-1">
-                <div>
-                  <h5 className="font-size-14 mb-1">James Nix</h5>
-                  <p className="font-size-13 text-muted mb-0">Full Stack Developer</p>
-                </div>
-              </div>
-            </div>
-          </a>
-          <a href="#" className="list-group-item list-group-item-action">
-            <div className="d-flex align-items-center">
-              <div className="avatar-sm flex-shrink-0 me-3">
-                <img src="assets/images/users/avatar-3.jpg" alt className="img-thumbnail rounded-circle" />
-              </div>
-              <div className="flex-grow-1">
-                <div>
-                  <h5 className="font-size-14 mb-1">Darlene Smith</h5>
-                  <p className="font-size-13 text-muted mb-0">UI/UX Designer</p>
-                </div>
-              </div>
-            </div>
-          </a>
-          <a href="#" className="list-group-item list-group-item-action">
-            <div className="d-flex align-items-center">
-              <div className="avatar-sm flex-shrink-0 me-3">
-                <div className="avatar-title bg-light-subtle text-light rounded-circle font-size-22">
-                  <i className="bx bxs-user-circle" />
-                </div>
-              </div>
-              <div className="flex-grow-1">
-                <div>
-                  <h5 className="font-size-14 mb-1">William Swift</h5>
-                  <p className="font-size-13 text-muted mb-0">Backend Developer</p>
-                </div>
-              </div>
-            </div>
-          </a>
+        <div>
+          <ul className="list-unstyled mb-0">
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">License No</h5>
+                  <p className="font-size-13 text-muted mb-0">T024223</p>
+            </li>
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">Vehicle Class</h5>
+                  <p className="font-size-13 text-muted mb-0">2024-02-25</p>
+            </li>
+            <li className='mb-2'>
+            <h5 className="font-size-14 mb-1">Note</h5>
+                  <p className="font-size-13 text-muted mb-0">O+, 56kg</p>
+            </li>
+           
+          </ul>
         </div>
       </div>
       {/* end card body */}
